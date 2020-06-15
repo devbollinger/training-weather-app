2098b98653b0665bfe7a908505caef2b41e0657c
David Bollinger <d.bollinge@gmail.com>
2020-06-14 21:25:19 -0400

    add humidity data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error){
             callback('Unable to find location', undefined)
         }else{
+            console.log(body.current)
             callback(
                 undefined,
-                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + ' The humidity is: ' + body.current.humidity
             )
         }
 
